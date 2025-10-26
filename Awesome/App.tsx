23b11d4dffa7b1f38aa7373a59471b320af96c32
neel20409 <bhattneel2004@gmail.com>
2025-10-26 18:00:04 +0530

    First React-native app

---
+++ Awesome/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+  View,
+  Text,
+  
+} from 'react-native'
+
+function App(){
+ return(
+ 
+    <View>
+     <Text>Hello  this is neel </Text>
+     <Text>Hello World</Text>
+     <Text>Hello World</Text>
+    </View>
+ 
+ )
+}
+
+export default App;
